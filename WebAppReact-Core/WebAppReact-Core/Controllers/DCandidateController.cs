fbf30b3c2debb45bae6951ecf3cc19cea3a87abe
Niti Modi <nitimodi@microsoft.com>
2021-12-12 19:03:44 +0530

    Hey added commit

--- WebAppReact-Core/WebAppReact-Core/Controllers/DCandidateController.cs
+++ WebAppReact-Core/WebAppReact-Core/Controllers/DCandidateController.cs
@@ -81,6 +81,7 @@ namespace WebAppReact_Core.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetDCandidate", new { id = dCandidate.id }, dCandidate);
+            //added data
         }
 
         // DELETE: api/DCandidate/5
